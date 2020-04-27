588486bb5f65f2028e1478f96e6d538d001424eb
Zach Ayers <zayers.dev@gmail.com>
2020-04-27 12:27:51 -0500

    Complete

--- video-streaming/client/src/components/App.js
+++ video-streaming/client/src/components/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
@@ -15,11 +15,13 @@ const App = () => (
     <Router history={history}>
       <Header />
       <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/create" exact component={StreamCreate} />
-        <Route path="/streams/view/:id" exact component={StreamView} />
-        <Route path="/streams/edit/:id" exact component={StreamEdit} />
-        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/create" exact component={StreamCreate} />
+          <Route path="/streams/:id" exact component={StreamView} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+        </Switch>
       </div>
     </Router>
   </div>
--- video-streaming/client/src/components/streams/StreamView.js
+++ video-streaming/client/src/components/streams/StreamView.js
@@ -1,5 +1,61 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
+import flv from 'flv.js';
+import { connect } from 'react-redux';
+import { fetchOneStream } from '../../actions';
 
-const StreamView = (props) => <div>StreamView</div>;
 
-export default StreamView;
+class StreamView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.props.fetchOneStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+export default connect(mapStateToProps, { fetchOneStream })(StreamView);
